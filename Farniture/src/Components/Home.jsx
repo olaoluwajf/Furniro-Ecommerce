0cfc8e02daf2ec5e5d63ea4974e4eb53b0fb487f
kenenisa1 <kenenisamiesob@gmail.com>
2025-05-14 23:42:46 +0300

    Add styling on Home component

--- Farniture/src/Components/Home.jsx
+++ Farniture/src/Components/Home.jsx
@@ -3,11 +3,14 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-          <section className="   bg-[url('./assets/f.jpeg')] bg-no-repeat bg-cover bg-fixed bg-center h-screen ">
-            <div className="bg-amber-400 items-center ml-[100px] p-5 rounded-2xl justify-end  ">
-              <h5>New Arrival</h5>
-              <h1 className="h1">Discover Our New Collection</h1> 
-              <p className="">Discover premium furniture crafted for modern living. Explore
+          <section className="flex-col absolute inset-0  justify-between bg-[url('./assets/f.jpeg')] bg-no-repeat bg-cover bg-fixed bg-center h-screen ">
+             <div>
+
+             </div>
+            <div className="bg-amber-400 items-center ml-[750px] mt-[100px] p-5 rounded-2xl justify-end flex flex-col md:w-1/2 space-y-6 ">
+              <h5 className='text-lg font-medium text-gray-600'>New Arrival</h5>
+              <h1 className="text-4xl md:text-5xl font-bold tracking-tight">Discover Our New Collection</h1> 
+              <p className="text-gray-600 max-w-md">Discover premium furniture crafted for modern living. Explore
                  our curated collection of sofas, beds, tables, and more—blending 
                  timeless design with exceptional quality. Transform your space
                   with pieces that inspire</p>
