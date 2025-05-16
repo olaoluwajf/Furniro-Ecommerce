c7c864eb221c50c9dab6baf230258fcccd1a1113
kenenisa1 <kenenisamiesob@gmail.com>
2025-05-16 10:40:14 +0300

    Integrate Room into App with data props

--- Farniture/src/App.jsx
+++ Farniture/src/App.jsx
@@ -1,6 +1,7 @@
 import Home from "./Components/Home";
 import Footer from './Components/Footer'
 import Header from './Components/Header'
+import Room from "./Components/Room";
 
 function App() {
 
@@ -8,6 +9,11 @@ function App() {
     <>
         <Header/>
         <Home/>
+        <div className="flex flex-row text-center justify-center px-4 py-4 ">
+          <Room name="Dining" />
+          <Room name="Living"/>
+          <Room name="Bedroom"/>
+        </div>
         <Footer/>
     </>
   )
