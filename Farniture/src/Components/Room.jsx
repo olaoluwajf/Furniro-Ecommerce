356db24d967f96bad74e1d845229ee6f4428e6ab
kenenisa1 <kenenisamiesob@gmail.com>
2025-05-17 23:56:46 +0300

    Add Datas to Room component using map method

--- Farniture/src/Components/Room.jsx
+++ Farniture/src/Components/Room.jsx
@@ -1,11 +1,18 @@
-
+import Datas from '../Datas'
 import Room1 from '../assets/proimg1.png'
 
-const Room = ({name}) => {
+const Room = () => {
   return (
     <div>
-         <img src={Room1} alt="" />
-            {name}
+         {Datas.map((item, index)=> 
+         (
+          <div key={index}>
+            <img src={item.img} alt="item.name" />
+            <p className=''>{item.name}</p>
+          </div> 
+         ))
+
+         }
     </div>
   )
 }
