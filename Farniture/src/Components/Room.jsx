c10637f5b3d49a8245aee9820cd94a1266a248ba
kenenisa1 <kenenisamiesob@gmail.com>
2025-05-18 01:11:02 +0300

    Add Header to Room component

--- Farniture/src/Components/Room.jsx
+++ Farniture/src/Components/Room.jsx
@@ -2,16 +2,22 @@ import {Rooms} from '../Datas'
 
 const Room = () => {
   return (
-    <div className='flex flex-row text-center justify-center px-4 py-4'>
-         {Rooms.map((item, index)=> 
-         (
+    <div>
+      <div className="container mx-auto">
+          <h1 className='text-3xl font-bold text-center mb-8'>Browse the Range</h1>
+          <p className="text-center ">Choose your favorite. Enjoy comfortable sleep</p>
+        </div>
+      <div className='flex flex-row text-center justify-center px-4 py-4'>
+        {Rooms.map((item, index)=> 
+        (
           <div key={index}>
             <img src={item.img} alt={item.name} />
-            <p className=''>{item.name}</p>
+            <p className='py-4 text-center font-bold'>{item.name}</p>
           </div> 
-         ))
+        ))
 
-         }
+        }
+      </div>
     </div>
   )
 }
