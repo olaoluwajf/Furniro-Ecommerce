730d586f99d93d0d325dac990e88cf98e8274c8d
kenenisa1 <kenenisamiesob@gmail.com>
2025-05-18 23:45:23 +0300

    Add Images datas to Datas file

---
+++ Farniture/src/Datas.js
@@ -0,0 +1,19 @@
+
+import catimg1 from './assets/catimg1.png';
+import Rectangle25 from './assets/Living.png';
+import Rectangle26 from './assets/Bedroom.png'
+
+export const Rooms=[
+        {name: 'Dining',
+         img: catimg1
+        },
+        {
+            name: 'Living',
+            img: Rectangle25
+        },
+        {
+            name: ' Bedroom',
+            img:  Rectangle26
+        }
+
+    ]
