084616648b32d713f5e9a874c69206366754c419
kenenisa1 <kenenisamiesob@gmail.com>
2025-05-19 18:04:17 +0300

    Add new products to Datas

--- Farniture/src/Datas.js
+++ Farniture/src/Datas.js
@@ -2,6 +2,9 @@
 import catimg1 from './assets/catimg1.png';
 import Rectangle25 from './assets/Living.png';
 import Rectangle26 from './assets/Bedroom.png'
+import Pro1 from './assets/proimg1.png'
+import pr2 from './assets/proimg3.png'
+import pr3 from './assets/proimg4.png'
 
 export const Rooms=[
         {name: 'Dining',
@@ -16,4 +19,51 @@ export const Rooms=[
             img:  Rectangle26
         }
 
-    ]
+    ];
+export const products=[
+    {
+        img: Pro1,
+        name: 'Syltherine',
+        price: 'Rp 1.500.000',
+        discount: 'Rp 3.500.000',
+        description: 'Stylish Cafe Chair'
+    } ,
+    {
+        img: pr2,
+        name: 'Leviosa',
+        price: 'Rp 2.500.000',
+        description: 'Stylish Cafe Chair'
+    } 
+     ,
+    {
+        img: pr3,
+        name: 'Solito',
+        price: 'Rp 7.000.000',
+        discount: 'Rp 14.000.000',
+        description: 'Luxury big sofa'
+    } 
+     ,
+    {
+        img: pr2,
+        name: 'Respira',
+        description: 'Outdoor bar table and stool',
+        price: 'Rp 1.500.000',
+        new: true    
+    },
+    {
+        img: pr2,
+        name: 'Syltherine',
+        price: 'Rp 1.500.000',
+        discount: 'Rp 3.500.000',
+        description: 'Stylish Cafe Chair'
+    } 
+     ,
+    {
+        img: pr2,
+        name: 'Syltherine',
+        price: 'Rp 1.500.000',
+        discount: 'Rp 3.500.000',
+        description: 'Stylish Cafe Chair'
+    } 
+     
+]
