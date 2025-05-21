1cfc7c30035a0e23f92ba7f966c4536530731f6b
kenenisa1 <kenenisamiesob@gmail.com>
2025-05-21 11:49:37 +0300

    Add button to Products component

--- Farniture/src/Components/Products.jsx
+++ Farniture/src/Components/Products.jsx
@@ -1,9 +1,13 @@
 import { products } from '../Datas';
+import Button from './Button';
 import AddToCart from './UI/AddToCart';
 
 const Products= () =>{
   return (
-  <div className='justify-center px-5 py-3'>
+  <div className='justify-center px-5 py-5'>
+    <div className='justify-center text-center text-5xl font-bold py-4 mb-5'>
+      <h1>Our Products</h1>
+    </div>
     <div className="grid grid-cols-1 cursor-pointer sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-center px-4">
       {products.map((item) => (
        <div className="relative group overflow-hidden border rounded-lg shadow-sm hover:shadow-md transition-all duration-300">
@@ -19,6 +23,12 @@ const Products= () =>{
           <span className='p-3 absolute bg-green-400 text-center text-white top-3 right-3 px-2 py-1 rounded-4xl w-[48px] h-[48px] z-32 font-bold justify-center'>
             new
           </span>  
+        )}
+        {item.disPrecent && (
+          <div className='p-3 absolute bg-red-700 text-center  text-white top-3 right-3 px-2 py-1 rounded-4xl w-[48px] h-[45px] z-32 font-bold justify-center'>
+            -{item.disPrecent}%
+          </div>
+
         )}
 
         <div className="p-4">
@@ -38,6 +48,9 @@ const Products= () =>{
         <AddToCart/>
       </div>
      ))}
+    </div>
+    <div className='justify-center text-center p-5 '>
+       <Button name='Show more'/>
     </div>
   </div>
 );
