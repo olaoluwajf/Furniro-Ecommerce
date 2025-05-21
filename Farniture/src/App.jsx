78bfb128be56890dd08fd099173f4899af27097a
kenenisa1 <kenenisamiesob@gmail.com>
2025-05-21 11:47:40 +0300

    Add RoomInspiration to App component for rendering

--- Farniture/src/App.jsx
+++ Farniture/src/App.jsx
@@ -2,6 +2,9 @@ import Home from "./Components/Home";
 import Footer from './Components/Footer'
 import Header from './Components/Header'
 import Room from "./Components/Room";
+import Products from "./Components/Products";
+import RoomInspiration from "./Components/RoomInspiration";
+import GalleryImages from "./Components/GalleryImage";
 
 function App() {
 
@@ -10,8 +13,13 @@ function App() {
         <Header/>
         <Home/>
         <div className="flex flex-row text-center justify-center px-4 py-4 ">
-           <Room/>
+           <Room />
         </div>
+        <div>
+          <Products/>
+        </div>
+        <RoomInspiration/>
+        <GalleryImages/>
         <Footer/>
     </>
   )
