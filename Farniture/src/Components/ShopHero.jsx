c29eef626c27d5916bbbef7a3a9999bd06b21f22
kenenisa1 <kenenisamiesob@gmail.com>
2025-05-28 21:59:41 +0300

    Add products to SHop page using products component

--- Farniture/src/Components/ShopHero.jsx
+++ Farniture/src/Components/ShopHero.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import { FaGreaterThan } from 'react-icons/fa'
+import Products from './Products'
+import Shop from './Shop'
 const ShopHero = () => {
   return (
-    <div className=" bg-[url('./assets/Rectangle1.png')] items-center text-center py-20 justify-center">
-        <h1 className='text-center  text-4xl font-bold'>Shop</h1>
+    <div>
+    <div className=" bg-[url('./assets/Rectangle1.png')] text-center py-20 justify-center">
+        <h1 className='text-center justify-center text-4xl font-bold'>Shop</h1>
         <div className='text-center flex items-center'>
             <div className='flex items-center text-center'>
                 <a href="" className='font-bold text-center'>Home</a> 
-                <FaGreaterThan className='text-center'/>
+                <FaGreaterThan className='justify-center text-[10px]'/>
             </div>
             <span className='text-center justify-center'>Shop</span>
+        </div>
+    </div>
+    <Shop/>
+    <div>
+          <Products/>
+          <Products/>
+          <Products/>
+          <Products/>
         </div>
     </div>
   )
