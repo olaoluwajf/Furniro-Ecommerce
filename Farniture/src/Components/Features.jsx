e58d88cdff4813e95773cb6689f2ecb80e5230fd
kenenisa1 <kenenisamiesob@gmail.com>
2025-06-24 16:59:41 +0300

    Add responsiveness to Features component

--- Farniture/src/Components/Features.jsx
+++ Farniture/src/Components/Features.jsx
@@ -4,26 +4,27 @@ import { FaShippingFast } from "react-icons/fa";
 import { FiCheckCircle } from 'react-icons/fi';
 import { FcCustomerSupport } from "react-icons/fc";
 import Featur from './UI/Featur';
+import { Link } from 'react-router-dom';
 
 const Features = () => {
   return (
     <div className='bg-amber-50 p-[100px] mt-5'>
-        <div className='flex justify-center mx-2 px-3'>
-            <div className='px-4'>
+        <div className='flex flex-col xl:flex-row lg:flex-row md:flex-row justify-center mx-2 px-3'>
+            <Link className='px-4'>
                  <Featur p='Crafted from top materials' span='High Quality' icon={<GrAchievement/>}/>
-            </div>
+            </Link>
 
-            <div className='px-4'>
+            <Link className='px-4'>
                 <Featur p='Over two years' span='Warranty Protection' icon={<FiCheckCircle/>}/>
-            </div>
+            </Link>
 
-            <div>
+            <Link>
                 <Featur p='Crafted from top materials' span='High Quality' icon={<FaShippingFast/>}/>
-            </div>
+            </Link>
 
-            <div>
+            <Link>
                 <Featur p='Dedicated Support' span='24/7 support' icon={<FcCustomerSupport/>}/>
-            </div>
+            </Link>
         </div>
     </div>
   )
