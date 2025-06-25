2935f96c52e2f70fd21a6a26eff63b441b1daf85
kenenisa1 <kenenisamiesob@gmail.com>
2025-06-25 15:32:39 +0300

    Add responsiveness to Shop component using Tailwindcss

--- Farniture/src/Components/Shop.jsx
+++ Farniture/src/Components/Shop.jsx
@@ -1,24 +1,25 @@
  import React from 'react'
  import {HiViewGrid} from 'react-icons/hi'
 import { FaSlidersH, FaBars } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
  
  const Shop = () => {
    return (
-     <div className='flex justify-between p-4 bg-amber-100 items-center'>
-        <div className='flex items center px-3'>
-            <div className='flex items-center px-3'>
+     <div className='flex flex-col md:flex-row xl:flex-row lg:flex-row sm:gap-y-5 justify-between p-4 bg-amber-100 items-center'>
+        <div className='flex justify-center lg:flex-row xl:flex-row items-center px-3 py-5'>
+            <Link className='flex py-3 gap-1 items-center px-3'>
                 <FaSlidersH className='text-2xl mx-2'/> Filter
-            </div>
-            <div className='flex items-center px-3'>
+            </Link>
+            <Link className='flex py-3 items-center px-3'>
                 <HiViewGrid className='text-2xl mx-2'/> <FaBars className='text-2xl'/>
-            </div>
+            </Link>
             <p>Showing  1-16 of 32 results</p>
         </div>
 
-        <div className='flex items-center px-3 justify-between'>
-            Show <span className='p-3 bg-amber-50'>16</span>
-            <div className='flex items-center mx-2'>
-                Short by <span className='bg-stone-50 p-3'>Default</span>
+        <div className='flex items-center px-3 justify-between gap-1 text-[15px]'>
+            Show <Link className='p-3 bg-amber-50'>16</Link>
+            <div className='flex items-center mx-2 gap-1 text-[15px]'>
+                Short by <Link className='bg-stone-50 p-3'>Default</Link>
             </div>
         </div>
      </div>
