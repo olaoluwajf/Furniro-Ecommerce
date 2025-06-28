aef11f623f6c13b534a597d6c26ab7966b410573
kenenisa1 <kenenisamiesob@gmail.com>
2025-06-28 17:55:08 +0300

    Add Some links and spaces for detail information of products to ProductDetail component

--- Farniture/src/Components/ProductDetail.jsx
+++ Farniture/src/Components/ProductDetail.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaEllipsisV, FaGreaterThan } from 'react-icons/fa'
 
-const ProductDetail = () => {
+const ProductDetail = (props) => {
   return (
     <div>
-        <h2>ProductDetail</h2>
+        <div className='flex flex-row p-3 gap-x-1 bg-amber-100' >
+          <Link to='/Home' >Home</Link>
+          <FaGreaterThan/>
+          <Link to='/ShopHero'>Shop</Link>
+          <FaGreaterThan/>
+          <FaEllipsisV/>
+          <span>{props.ProductName}</span>
+
+        </div>
+        <section>
+          <div>
+            <div>
+              {props.Image1}
+              {props.Image1}
+              {props.Image1}
+
+            </div>
+            
+            <div>
+              {props.MainImage}
+            </div>
+          </div>
+
+          <div>
+            <h1>{props.ProductName}</h1>
+            <h3>Rs.{props.price}</h3>
+          </div>
+        </section>
     </div>
   )
 }
