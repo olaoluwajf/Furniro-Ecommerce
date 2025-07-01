32c320c003c3d0c93ccef452e88311d0b606317e
kenenisa1 <kenenisamiesob@gmail.com>
2025-07-01 15:53:21 +0300

    Add product route with its id to App.jsx file for seeing product detail information from productDetail page

--- Farniture/src/App.jsx
+++ Farniture/src/App.jsx
@@ -1,28 +1,25 @@
+import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
-import Footer from './Components/Footer'
-import Header from './Components/Header'
-import Room from "./Components/Room";
+import Footer from './Components/Footer';
+import Header from './Components/Header';
 import Products from "./Components/Products";
-import RoomInspiration from "./Components/RoomInspiration";
-import GalleryImages from "./Components/GalleryImage";
-import Shop from "./Components/Shop";
 import ShopHero from "./Components/ShopHero";
-import {Route, Routes } from "react-router-dom";
 import ProductDetail from "./Components/ProductDetail";
 
-function App() {
 
+function App() {
   return (
     <>
       <Header/>
       <Routes>
-              <Route path="/" element={<Home/>}>  </Route>
-              <Route path="/ShopHero" element={<ShopHero/>}></Route>
-              <Route path="/ProductDetail" element={<ProductDetail/>}></Route>
+        <Route path="/" element={<Home/>} />
+        <Route path="/ShopHero" element={<ShopHero/>} />
+        <Route path="/product/:id" element={<ProductDetail/>} />
+        <Route path="/shop" element={<Products/>} />
       </Routes>
       <Footer/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
