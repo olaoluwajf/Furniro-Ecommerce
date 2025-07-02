a624e4f0cf69620aca2b4f62f80b5270e2a0ed9b
kenenisa1 <kenenisamiesob@gmail.com>
2025-07-02 17:49:12 +0300

    Add Contact page with hero section

---
+++ Farniture/src/Components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from './Hero'
+import Logo from '../assets/logo.png'
+
+const Contact = () => {
+  return (
+    <div>
+        <Hero PageName="Contact" img={Logo}/>
+    </div>
+  )
+}
+
+export default Contact
