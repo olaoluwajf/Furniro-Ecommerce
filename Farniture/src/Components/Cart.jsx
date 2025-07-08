aa84f14c3eead4ebb610c20affdc48f2d88b209c
kenenisa1 <kenenisamiesob@gmail.com>
2025-07-08 22:37:38 +0300

    Add responsiveness to Cart page using tailwindcss

--- Farniture/src/Components/Cart.jsx
+++ Farniture/src/Components/Cart.jsx
@@ -12,45 +12,45 @@ const Cart = () => {
   return (
     <div className='justify-center'>
         <Hero PageName="Cart" img={Logo}/>
-        <div className='ml-10 mr-10 mt-4 flex flex-col lg:flex-row xl:flex-row gap-2 justify-center'>
-          <div className='flex flex-col '>
+        <div className='ml-10 mr-10 mt-4 flex flex-col md:flex-row lg:flex-row xl:flex-row gap-2 justify-center mb-5'>
+          <div className='hidden md:flex-row  flex-col mr-4 lg:block xl:block'>
             <div className='flex flex-row gap-10  text-stone-900 bg-amber-50 p-5 px-30'>
               <span className='mr-5 ml-5 pl-5 '>Product</span>
               <span className='px-4'>Price</span>
               <span className='px-2 pl-4'>Quantity</span>
               <span className='px-5'>Subtotal</span>
             </div>
-            <div className='flex flex-row gap-5 pt-5  items-center'>
-              <div className='bg-amber-50 p-3 h-20 w-30 items-center justify-center'>
+            <div className='flex flex-row gap-5 pt-5   items-center text-stone-500'>
+              <div className='bg-amber-50 p-3 h-20  w-30 items-center justify-center'>
                 <img src='' alt="img" className='mr-5'/>
 
               </div>
               
-              <p className=''>product.name</p>
+              <p className='mr-3'>product.name</p>
               <p>product.price</p>
-              <p className='border p-1'>product.quantity</p>
-              <p>product.subtotal</p>
+              <p className='border px-2 ml-8'>1</p>
+              <p className='ml-10'>product.subtotal</p>
               <div>
-                <FiTrash className='text-2xl cursor-pointer hover:text-stone-600'/>
+                <FiTrash className='text-2xl cursor-pointer hover:text-stone-600 text-amber-300 font-bold'/>
               </div>
 
             </div>
 
           </div>
 
-          <div className='flex flex-col rounded bg-amber-50  px-10   '>
+          <div className='flex flex-col rounded bg-amber-50 md:w-full  px-10 md:justify-center sm:justify-center  '>
             <h1 className='text-center text-2xl py-1 mb-3 font-bold '>Cart Total</h1>
-            <div className='flex flex-col lg:flex-row xl:flex-row gap-6 ml-3 mt-4 mb-3'>
-              <p>SubTotal</p>
-              <p>{ }
+            <div className='flex flex-row gap-6 ml-3 mt-4 mb-3'>
+              <p className='flex'>Subtotal</p>
+              <p className='ml-5 flex text-stone-400'>{product }
                 price
               </p>
             </div>
-            <div className='flex flex-col lg:flex-row xl:flex-row gap-6 ml-3'>
+            <div className='flex flex-row gap-6 ml-3'>
               <p>
                 Total
               </p>
-              <p>
+              <p className='ml-10 text-amber-600'>
                 { }
                 total
               </p>
@@ -58,7 +58,7 @@ const Cart = () => {
             </div>
             <div className='flex-col gap-3 p-10 mt-5'>
                 <Link className='rounded-xl p-2 px-7 border'>
-                  Add to cart
+                  Check Out
                 </Link>
             </div>
 
