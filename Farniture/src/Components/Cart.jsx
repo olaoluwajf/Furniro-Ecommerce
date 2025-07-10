aba415f811616c7c7013ad902e59cae8c68d996e
kenenisa1 <kenenisamiesob@gmail.com>
2025-07-10 23:02:00 +0300

    Add styling and responsiveness to Cart page

--- Farniture/src/Components/Cart.jsx
+++ Farniture/src/Components/Cart.jsx
@@ -38,15 +38,15 @@ const Cart = () => {
 
           </div>
 
-          <div className='flex flex-col rounded bg-amber-50 md:w-full  px-10 md:justify-center sm:justify-center  '>
+          <div className='flex flex-col rounded bg-amber-50 px-10 md:justify-center sm:justify-center w-full md:w-full text-center lg:w-120 xl:w-140 '>
             <h1 className='text-center text-2xl py-1 mb-3 font-bold '>Cart Total</h1>
-            <div className='flex flex-row gap-6 ml-3 mt-4 mb-3'>
+            <div className='flex flex-row gap-6 ml-3 mt-4 mb-3 justify-center'>
               <p className='flex'>Subtotal</p>
               <p className='ml-5 flex text-stone-400'>{product }
                 price
               </p>
             </div>
-            <div className='flex flex-row gap-6 ml-3'>
+            <div className='flex flex-row gap-6 ml-3 justify-center'>
               <p>
                 Total
               </p>
@@ -57,7 +57,7 @@ const Cart = () => {
               
             </div>
             <div className='flex-col gap-3 p-10 mt-5'>
-                <Link className='rounded-xl p-2 px-7 border'>
+                <Link to='/Checkout' className='rounded-xl p-2 px-7 border'>
                   Check Out
                 </Link>
             </div>
