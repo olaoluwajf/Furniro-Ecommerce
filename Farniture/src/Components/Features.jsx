cf8ba5fda9d50402acffc1be7a2243ab3c36988b
kenenisa1 <kenenisamiesob@gmail.com>
2025-07-12 22:57:19 +0300

    Add styling to Features component  using tailwindcss

--- Farniture/src/Components/Features.jsx
+++ Farniture/src/Components/Features.jsx
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom';
 
 const Features = () => {
   return (
-    <div className='bg-amber-50 p-[100px] mt-5'>
+    <div className='bg-amber-50 p-[80px] mt-5 items-center'>
         <div className='flex flex-col xl:flex-row lg:flex-row md:flex-row justify-center mx-2 px-3'>
             <Link className='px-4'>
                  <Featur p='Crafted from top materials' span='High Quality' icon={<GrAchievement/>}/>
