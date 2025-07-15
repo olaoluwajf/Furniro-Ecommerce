07f924bea4663a1360240fcb8bb2b9bb9ed64090
kenenisa1 <kenenisamiesob@gmail.com>
2025-07-15 15:19:43 +0300

    Add Blog page as a component

---
+++ Farniture/src/Components/Blog.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from './Hero'
+
+const Blog = () => {
+  return (
+    <div>
+        <Hero pageName='Blog'/>
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Blog
