b20876a64f667eaa3df0ae8096d8af273fa00135
kenenisa1 <kenenisamiesob@gmail.com>
2025-07-15 16:42:24 +0300

    refactor(Blog): Implement Post component for blog entries, simplifying page structure

--- Farniture/src/Components/Blog.jsx
+++ Farniture/src/Components/Blog.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
 import Hero from './Hero'
-
+import Post from './Post'
 const Blog = () => {
   return (
     <div>
-        <Hero pageName='Blog'/>
-        <div>
-
+        <Hero PageName='Blog'/>
+        <div className='flex flex-col lg:flex-row xl:flex-row justify-center items-center gap-5 m-5 mt-20'>
+            <div className='side 1'>
+                <div>
+                    <Post />
+                </div>
+                <div>
+                    <Post />
+                </div>
+                <div>
+                    <Post />
+                </div>
+            </div>
+            <div>
+            </div>
         </div>
     </div>
   )
