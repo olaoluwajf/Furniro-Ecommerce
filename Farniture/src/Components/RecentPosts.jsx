0b17c42606c250fb2e05c74d89d4c31e799be076
kenenisa1 <kenenisamiesob@gmail.com>
2025-07-17 23:43:58 +0300

    Remove RecentPosts component using map method with Datas instead of calling as a component for every posts

--- Farniture/src/Components/RecentPosts.jsx
+++ Farniture/src/Components/RecentPosts.jsx
@@ -1,17 +1,9 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import { BlogPosts } from '../Datas';
 
-const RecentPosts = (props) => {
+const RecentPosts = () => {
   return (
-    <div className='m-4 mb-6 flex flex-col gap-5'>
-        <h1 className='font-bold text-2xl m-3'>
-            Recent Photos</h1>
-        <div className='flex flex-row items-center text-left'>
-            <img src={props.image} alt="" />
-            <div className='flex flex-col  items-center '>
-                <h2 className='font-semibold w-40 m-2 mb-1'> {props.description}</h2>
-                <span className='text-sm w-30 flex'>{props.dayInfo}</span>
-            </div>
-        </div>
+    <div className='m-4 mb-6 flex flex-col  gap-5'>
     </div>
   )
 }
