b10e3a72cb883815183967356a99a734c0cb7ba1
kenenisa1 <kenenisamiesob@gmail.com>
2025-07-17 23:22:07 +0300

    Refactor(Blog) : Add Posts to Post component using map method instead of using prop

--- Farniture/src/Components/Post.jsx
+++ Farniture/src/Components/Post.jsx
@@ -1,13 +1,16 @@
 import BlogDescription from './BlogDescription'
 import {Link} from 'react-router-dom'
+import { Posts } from '../Datas'
 
-const Post = (props) => {
+const Post = () => {
   return (
-    <div className='mt-10 mb-10 flex flex-col rounded-lg'>
-      <img src={props.image} alt="" />
-      <BlogDescription />
-      <h1 className='text-2xl m-3'>{props.header}</h1>
-      <p className='text-gray-500 m-5'>
+    <>
+    {Posts.map((post) => (
+    <Link  className='mt-10 mb-10 flex flex-col rounded-lg'>
+      <img src={post.img} alt="" />
+      <BlogDescription  />
+      <h1 className='text-2xl m-3'>{post.header}</h1>
+      <p className='text-gray-500 m-5 '>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium aliquid doloremque, <br /> in suscipit voluptates eos fugiat ratione architecto vitae omnis delectus sunt esse nesciunt beatae <br /> modi, incidunt cupiditate dolore. Doloribus.
           Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit. Doloribus, cumque.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, cumque.
       </p>
@@ -15,7 +18,9 @@ const Post = (props) => {
       <Link to='/Post ' className='font-bold text-lg hover:underline underline p-2 m-4'>
               Read more
       </Link>
-    </div>
+    </Link>
+     ))}
+     </>
   )
 }
 
