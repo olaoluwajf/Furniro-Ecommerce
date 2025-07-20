2e3d1411796df12d06718065626f463d4e06bb84
kenenisa1 <kenenisamiesob@gmail.com>
2025-07-20 21:54:54 +0300

    Add Responsiveness to Button component using tailwindcss

--- Farniture/src/Components/Button.jsx
+++ Farniture/src/Components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = ({name}) => {
   return (
     <div > 
-        <button className='rounded bg-amber-600 text-white font-bold border-[1px] hover:bg-amber-700 px-10 py-3  cursor-pointer'>
+        <button className='rounded text-sm   bg-amber-600 text-white font-bold border-[1px] hover:bg-amber-700 px-4 py-3 sm:px-4 md:px-6 lg:px-8 xl:px-10  sm:text-sm sm:py-4 cursor-pointer '>
             {name}
         </button>
     </div>
