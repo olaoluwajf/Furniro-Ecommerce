07541318236b81443b746c326e4b14e68d8ddf1a
kenenisa1 <kenenisamiesob@gmail.com>
2025-07-21 11:23:27 +0300

    Add responsiveness, styling using tailwindcss

--- Farniture/src/Components/RoomDetail.jsx
+++ Farniture/src/Components/RoomDetail.jsx
@@ -20,9 +20,9 @@ const RoomDetail = () => {
         <Hero PageName="RoomDetail"/>
         <div className='m-4 justify-center'>
             <h1 className='text-5xl  font-bold text-center mb-8'>{Room.name}</h1>
-            <p className='text-center font-semibold text-2xl text-amber-700'>Explore the details of {Room.name} and find your perfect fit.</p>
-            <div className='w-full justify-center p-10  flex items-center mt-10 bg-white shadow-2xl'>
-                <img src={Room.img} alt={Room.name} />
+            <p className='text-center font-semibold text-2xl text-green-700'>Explore the details of {Room.name} and find your perfect fit.</p>
+            <div className='w-full justify-center p-10  flex flex-col lg:flex-row xl:flex-row items-center mt-10 bg-white shadow-2xl'>
+                <img src={Room.img} alt={Room.name} className='w-[50%]'/>
                 <div className='justify-center m-5'>
                     <p className='text-lg'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut, illo amet at pariatur <br />veritatis rem, numquam, velit eum doloribus iusto eos aliquid necessitatibus <br />tenetur nobis a quam ipsam unde libero! Lorem ipsum dolor sit, amet consectetur <br />adipisicing elit. Ipsa, totam perspiciatis! Blanditiis cumque repellat dicta nisi qui <br />eum eos eius pariatur. Delectus saepe totam, minus eveniet deleniti impedit velit doloremque?</p>
                 </div>
